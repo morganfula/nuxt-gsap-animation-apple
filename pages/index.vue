2d134e979050d56715b79e9ff7f1c4582e765b81
morganfula <morgan.fula@me.com>
2021-02-01 09:23:26 +0100

    scroll magic install

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <h1 id="title">
+      Hello World
+    </h1>
+  </div>
 </template>
 
 <script>
-export default {};
+import { gsap } from "gsap";
+
+export default {
+  mounted: function() {
+    this.startAnimations();
+  },
+  methods: {
+    startAnimations: function() {
+      gsap.to("#title", { duration: 1, color: "red" });
+    }
+  }
+};
 </script>
 
 <style></style>
